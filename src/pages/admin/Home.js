79233bfccd8528b1eac3d544c974998e4d1e4b1d
Roshan <rxgody@gmail.com>
2023-06-01 22:39:47 +0530

    admin-page

--- src/pages/admin/Home.js
+++ src/pages/admin/Home.js
@@ -1,5 +1,5 @@
 import '../../App.css'
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {faFingerprint} from  '@fortawesome/free-solid-svg-icons'
 import Layout from "../../components/Layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
